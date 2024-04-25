26dd6f6075694e4e8486562217549770cee76977
Evemose <rotan.olexandr@lll.kpi.ua>
2024-04-26 02:22:03 +0300

    Updated GetAdressDto to match entity

--- src/main/java/org/users/core/model/dto/PostAddressDTO.java
+++ src/main/java/org/users/core/model/dto/PostAddressDTO.java
@@ -1,10 +1,7 @@
 package org.users.core.model.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.*;
 import org.users.core.validation.Post;
 
 @Schema(description = "The address of a user",
@@ -22,7 +19,7 @@ public record PostAddressDTO(
         @NotBlank(groups = Post.class) String street,
         @NotBlank(groups = Post.class) String city,
         @NotBlank(groups = Post.class) String country,
-        @NotNull(groups = Post.class) @Min(10000) @Max(99999) Integer zipCode // 5-digit zip code,
+        @NotNull(groups = Post.class) @Pattern(regexp = "\\d{5}")  String zipCode // 5-digit zip code,
         // validation could be replaced with @Range(min = 10000, max = 99999), but swagger does not support it
 ) {
 }
