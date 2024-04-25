0e2fe7b6c147ffeea2b86f3e728df3b215c5bafa
Evemose <rotan.olexandr@lll.kpi.ua>
2024-04-25 20:41:47 +0300

    Updated Exception handlers to log errors and be visible in swagger

--- src/main/java/org/users/core/GlobalExceptionHandler.java
+++ src/main/java/org/users/core/GlobalExceptionHandler.java
@@ -1,15 +1,30 @@
 package org.users.core;
 
 import jakarta.persistence.EntityNotFoundException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<Void> handleEntityNotFound() {
-        return ResponseEntity.notFound().build();
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public void handleEntityNotFound(EntityNotFoundException ex) {
+        // entity not found is a common exception, so log as warning
+        log.warn(ex.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handleException(Exception ex) {
+        // log all other exceptions as errors
+        log.error("An error occurred", ex);
+        // returning manually to not expose in swagger
+        // forbidden status code is used to not expose internal errors
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
     }
 }
