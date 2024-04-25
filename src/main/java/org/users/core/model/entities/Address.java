6c8d621efbb85c659c0ea4c36a7cc6e921ca046a
Evemose <rotan.olexandr@lll.kpi.ua>
2024-04-26 02:21:17 +0300

    Replaced @Range with @Pattern cause zipCode could have leading zeros

--- src/main/java/org/users/core/model/entities/Address.java
+++ src/main/java/org/users/core/model/entities/Address.java
@@ -3,6 +3,7 @@ package org.users.core.model.entities;
 import jakarta.persistence.Embeddable;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Positive;
 import lombok.NonNull;
 import org.hibernate.validator.constraints.Range;
@@ -13,6 +14,6 @@ public record Address(
         @NotBlank @NonNull String street,
         @NotBlank @NonNull String city,
         @NotBlank @NonNull String country,
-        @Range(min = 10000, max = 99999) @NotNull @NonNull Integer zipCode
+        @Pattern(regexp = "\\d{5}") @NotNull @NonNull String zipCode
 ) {
 }
