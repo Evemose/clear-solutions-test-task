fd1a79b9e85a79d975ed3104f712e84fcd9d7a6d
Evemose <rotan.olexandr@lll.kpi.ua>
2024-04-25 22:22:20 +0300

    Added custom date and time serialization format

---
+++ src/main/java/org/users/core/DateTimeSerializationConfig.java
@@ -0,0 +1,53 @@
+package org.users.core;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Configuration
+public class DateTimeSerializationConfig {
+    @Bean
+    public ObjectMapper objectMapper() {
+        var objectMapper = new ObjectMapper();
+        var timeModule = new JavaTimeModule();
+        timeModule.addSerializer(LocalDateTime.class, new CustomLocalDateTimeSerializer());
+        timeModule.addDeserializer(LocalDateTime.class, new CustomLocalDateTimeDeserializer());
+        timeModule.addSerializer(LocalDate.class, new CustomLocalDateSerializer());
+        timeModule.addDeserializer(LocalDate.class, new CustomLocalDateDeserializer());
+        objectMapper.registerModule(timeModule);
+        return objectMapper;
+    }
+
+    private static class CustomLocalDateTimeSerializer extends LocalDateTimeSerializer {
+        public CustomLocalDateTimeSerializer() {
+            super(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        }
+    }
+
+    private static class CustomLocalDateTimeDeserializer extends LocalDateTimeDeserializer {
+        public CustomLocalDateTimeDeserializer() {
+            super(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        }
+    }
+
+    private static class CustomLocalDateSerializer extends LocalDateSerializer {
+        public CustomLocalDateSerializer() {
+            super(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        }
+    }
+
+    private static class CustomLocalDateDeserializer extends LocalDateDeserializer {
+        public CustomLocalDateDeserializer() {
+            super(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        }
+    }
+}
