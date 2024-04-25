97fbc73afde580e2a5f2959f297cea95e39572d2
Evemose <rotan.olexandr@lll.kpi.ua>
2024-04-26 02:21:44 +0300

    Added DataIntegrityViolationException handling

--- src/main/java/org/users/core/GlobalExceptionHandler.java
+++ src/main/java/org/users/core/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
@@ -16,6 +17,10 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeParseException;
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 @Slf4j
@@ -35,7 +40,7 @@ public class GlobalExceptionHandler {
             description = "Returns a list of constraint violations",
             responseCode = "400"
     )
-    public ResponseEntity<String[]> handleConstraintDeclaration(ConstraintViolationException ex) {
+    public ResponseEntity<String[]> handleConstraintViolation(ConstraintViolationException ex) {
         // constraint violation exception is a common exception, so log as warning
         log.warn(ex.getMessage());
         return ResponseEntity.badRequest().body(ex.getConstraintViolations().stream()
@@ -72,6 +77,56 @@ public class GlobalExceptionHandler {
             }
         }
         return ResponseEntity.badRequest().build();
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<String[]> handleDataIntegrityViolation(DataIntegrityViolationException ex) {
+        if (ex.getCause() instanceof org.hibernate.exception.ConstraintViolationException constraintEx) {
+            // data integrity violation is a common exception, so log as warning
+            log.warn(ex.getMessage());
+            if (constraintEx.getMessage().contains("Unique index or primary key")) {
+                var fieldName = extractFieldName(constraintEx.getMessage());
+                return fieldName.map(s -> ResponseEntity.badRequest().body(
+                        new String[]{s + " must be unique, but a duplicate was found"
+                                + extractValue(constraintEx.getSQLException().getMessage()).map(v -> ": " + v).orElse("")}
+                )).orElseGet(() -> ResponseEntity.badRequest().build());
+            }
+        }
+        // usually this should be unreachable,
+        // as DataIntegrityViolationException is a wrapper for ConstraintViolationException
+        // because this is unexpected, log as error
+        log.error(ex.getMessage());
+        return ResponseEntity.badRequest().build();
+    }
+
+    /**
+     * Extracts the field name from the message of DataIntegrityViolationException
+     * @param message the message of the exception
+     * @return the field name if found
+     */
+    private Optional<String> extractFieldName(String message) {
+        var outerGroupMatcher = Pattern.compile("ON PUBLIC\\.\\w+\\([\\w\\s]+\\)").matcher(message);
+        if (outerGroupMatcher.find()) {
+            var snakeCase = outerGroupMatcher.group().split("\\(")[1].split("\\s")[0].strip().toLowerCase();
+            return Optional.of(Arrays.stream(snakeCase.split("_"))
+                    .map(s -> s.substring(0, 1).toUpperCase() + s.substring(1))
+                    .collect(Collectors.joining()));
+        }
+        return Optional.empty();
+    }
+
+    /**
+     * Extracts the value from the message of DataIntegrityViolationException
+     * @param message the message of the exception
+     * @return the value if found
+     */
+    private Optional<String> extractValue(String message) {
+        var outerGroupMatcher = Pattern.compile("\\sVALUES\\s*\\(\\s*/\\*\\s*\\d+\\s*\\*/.+\\)").matcher(message);
+        if (outerGroupMatcher.find()) {
+            return Optional.of(outerGroupMatcher.group().split("\\*/")[1].split("\\)")[0].strip());
+        }
+        return Optional.empty();
     }
 
     // better not to expose the actual type in the error message
