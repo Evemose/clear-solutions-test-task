7cf6d2371b480370e6bfd9442f4841023f2b1220
Evemose <rotan.olexandr@lll.kpi.ua>
2024-04-25 20:41:21 +0300

    Updated Address constructor

--- src/main/java/org/users/core/model/entities/Address.java
+++ src/main/java/org/users/core/model/entities/Address.java
@@ -1,17 +1,20 @@
 package org.users.core.model.entities;
 
 import jakarta.persistence.Embeddable;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.NonNull;
 import lombok.With;
+import org.hibernate.validator.constraints.Range;
 
 @Embeddable
 public record Address(
+        @Positive @NotNull @NonNull Integer houseNumber,
         @NotBlank @NonNull String street,
         @NotBlank @NonNull String city,
         @NotBlank @NonNull String country,
-        @Positive @NotNull @NonNull Long zipCode
+        @Range(min = 10000, max = 99999) @NotNull @NonNull Integer zipCode
 ) {
 }
