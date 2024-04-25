01262b6e83743339cd6fc2fc5fe178b1c66d0681
Evemose <rotan.olexandr@lll.kpi.ua>
2024-04-25 20:48:17 +0300

    Added @RequestBody annotation in controllers that i forgor to put earlier

--- src/main/java/org/users/core/UserController.java
+++ src/main/java/org/users/core/UserController.java
@@ -1,13 +1,18 @@
 package org.users.core;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.users.core.model.UserMapper;
 import org.users.core.model.dto.GetUserDTO;
 import org.users.core.model.dto.PostUserDTO;
-import org.users.core.model.entities.User;
+import org.users.core.validation.Post;
 
 import java.net.URI;
 import java.time.LocalDate;
@@ -23,20 +28,26 @@ public class UserController {
     @GetMapping("/{id}")
     public ResponseEntity<GetUserDTO> get(@PathVariable Long id) {
         return ResponseEntity.ok(
-                userService.findById(id).map(userMapper::toDto).orElseThrow(EntityNotFoundException::new)
+                userService.findById(id).map(userMapper::toDto).orElseThrow(
+                        () -> new EntityNotFoundException("User with id " + id + " not found")
+                )
         );
     }
 
     @GetMapping
+    @ApiResponse(
+            description = "Returns all users with a birthdate between the given dates",
+            responseCode = "200"
+    )
     public ResponseEntity<List<GetUserDTO>> getAll(@RequestParam(defaultValue = "1900-01-01") LocalDate minBirthdate,
-                                             @RequestParam(defaultValue = "9999-12-31") LocalDate maxBirthdate) {
+                                                   @RequestParam(defaultValue = "9999-12-31") LocalDate maxBirthdate) {
         return ResponseEntity.ok(
                 userService.findByBirthdateBetween(minBirthdate, maxBirthdate).stream().map(userMapper::toDto).toList()
         );
     }
 
     @PostMapping
-    public ResponseEntity<GetUserDTO> save(PostUserDTO userDTO) {
+    public ResponseEntity<GetUserDTO> save(@RequestBody @Validated(Post.class) PostUserDTO userDTO) {
         var saved = userService.save(userMapper.toEntity(userDTO));
         return ResponseEntity.created(URI.create("/users/%d".formatted(saved.getId())))
                 .body(userMapper.toDto(saved));
@@ -49,7 +60,7 @@ public class UserController {
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<GetUserDTO> update(@PathVariable Long id, PostUserDTO userDTO) {
+    public ResponseEntity<GetUserDTO> update(@PathVariable Long id, @RequestBody @Validated(Post.class) PostUserDTO userDTO) {
         if (!userService.existsById(id)) {
             throw new EntityNotFoundException();
         }
@@ -60,10 +71,12 @@ public class UserController {
     }
 
     @PatchMapping("/{id}")
-    public ResponseEntity<GetUserDTO> patch(@PathVariable Long id, PostUserDTO userDTO) {
+    public ResponseEntity<GetUserDTO> patch(@PathVariable Long id, @RequestBody PostUserDTO userDTO) {
         return ResponseEntity.ok(userMapper.toDto(
                 userService.save(
-                        userMapper.toEntity(userDTO, userService.findById(id).orElseThrow(EntityNotFoundException::new))
+                        userMapper.toEntity(userDTO, userService.findById(id).orElseThrow(
+                                () -> new EntityNotFoundException("User with id " + id + " not found")
+                        ))
                 )
         ));
     }
