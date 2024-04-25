3b1d12ceac96ab21ddec7a47e6e0a18097321bbc
Evemose <rotan.olexandr@lll.kpi.ua>
2024-04-25 22:29:17 +0300

    Updated PostAddressDTO to disable null assertions for patch

--- src/main/java/org/users/core/model/dto/PostAddressDTO.java
+++ src/main/java/org/users/core/model/dto/PostAddressDTO.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import org.users.core.validation.Post;
 
 @Schema(description = "The address of a user",
         example = """
@@ -17,11 +18,11 @@ import jakarta.validation.constraints.NotNull;
                 }
                 """)
 public record PostAddressDTO(
-        @NotNull @Min(1) Integer houseNumber,
-        @NotBlank String street,
-        @NotBlank String city,
-        @NotBlank String country,
-        @Min(10000) @Max(99999) Integer zipCode // 5-digit zip code,
+        @NotNull(groups = Post.class) @Min(1) Integer houseNumber,
+        @NotBlank(groups = Post.class) String street,
+        @NotBlank(groups = Post.class) String city,
+        @NotBlank(groups = Post.class) String country,
+        @NotNull(groups = Post.class) @Min(10000) @Max(99999) Integer zipCode // 5-digit zip code,
         // validation could be replaced with @Range(min = 10000, max = 99999), but swagger does not support it
 ) {
 }
