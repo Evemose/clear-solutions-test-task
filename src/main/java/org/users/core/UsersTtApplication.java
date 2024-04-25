a7b27d6d3d3d1375a1130cd24658442f85bc84e9
Evemose <rotan.olexandr@lll.kpi.ua>
2024-04-25 18:40:54 +0300

    Added entity auditing

--- src/main/java/org/users/core/UsersTtApplication.java
+++ src/main/java/org/users/core/UsersTtApplication.java
@@ -2,8 +2,10 @@ package org.users.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class UsersTtApplication {
 
     public static void main(String[] args) {
